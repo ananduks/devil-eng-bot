e7da4a8fe506090ad7465cd6ad10aaf6f17131c6
ananduks <52703105+ananduks@users.noreply.github.com>
2019-08-13 02:53:18 -0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,7 +24,7 @@ im a group manager bot maintained by  [this man](tg://user?id={}).
 
 I was built on python3 using the python-telegram-bot library.... 
 
-I am completely opensource. My Orginal code is available to you [here](https://t.me/anandus) 
+I am completely opensource. My Orginal code is available to you [here](https://github.com/ananduks/devil-eng-bot) 
 
 hit help for commands /help..
 
